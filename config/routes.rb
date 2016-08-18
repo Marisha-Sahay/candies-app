e4e257f4b25f0d2a9b9ecdee37add04b528a102b
Marisha Sahay <marishasahay@gmail.com>
2016-08-17 20:05:30 -0700

    Add namespace and version to api

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,19 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get '/candies', to: 'candies#index'
-  get '/candies/:id', to: 'candies#show'
-  post '/candies/create', to: 'candies#create'
-  patch '/candies/:id', to: 'candies#update'
-  delete '/candies/:id', to: 'candies#destroy'
+  namespace :api do
+    namespace :v1 do
+      get '/candies', to: 'candies#index'
+      get '/candies/:id', to: 'candies#show'
+      post '/candies/create', to: 'candies#create'
+      patch '/candies/:id', to: 'candies#update'
+      delete '/candies/:id', to: 'candies#destroy'
+    end
+    # namespace :v2 do
+    #   get '/candies', to: 'candies#index'
+    #   get '/candies/:id', to: 'candies#show'
+    #   post '/candies/create', to: 'candies#create'
+    #   patch '/candies/:id', to: 'candies#update'
+    #   delete '/candies/:id', to: 'candies#destroy'
+    # end
+  end
 end
