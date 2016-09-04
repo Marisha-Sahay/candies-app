bb64be67fe1b40ad161e2f09123f12ad0b71a6cd
Marisha Sahay <marishasahay@gmail.com>
2016-09-04 11:23:25 -0700

    Add to db from api

--- app/assets/javascripts/candies_ctrl.js
+++ app/assets/javascripts/candies_ctrl.js
@@ -2,14 +2,14 @@
   "use.strict";
 
   angular.module("app").controller("candiesCtrl", function($scope,$http) {
-  
-  $scope.candies = [];  
-  
+    
+    $scope.candies = [];  
+    
     $scope.setup = function() {
       $http.get("/api/v1/candies.json").then(function(response){
         $scope.candies = response.data;
-        console.log(response.data)
-      })
+        // console.log(response.data)
+      });
     };
 
     $scope.addCandy = function(newName,newExpiry,newPrice) {
@@ -18,11 +18,13 @@
         expiry: newExpiry,
         price: newPrice
       };
-      console.log(candy)
-      $scope.candies.push(candy);
-    }
+      $http.post("/api/v1/candies.json", candy).then(function(response){
+        console.log(candy);
+        $scope.candies.push(response.data);
+      });
+    };
 
-window.$scope = $scope;
+    window.$scope = $scope;
     
   });
 })();
