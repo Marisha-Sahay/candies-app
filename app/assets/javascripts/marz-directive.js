d87c8c7276785dfe958e04b55509c50e1408811b
Marisha Sahay <marishasahay@gmail.com>
2016-09-08 19:05:36 -0700

    Add custom directive with template

--- app/assets/javascripts/marz-directive.js
+++ app/assets/javascripts/marz-directive.js
@@ -1,6 +1,6 @@
 angular.module("app").directive("marzDirective",function(){
   return {
     restrict: 'E',
-    template: '<h1>This is mars directive</h1><h2>I can add this text anywhere</h2>'
+    templateUrl: "marz_directive.html"
   }
 });
